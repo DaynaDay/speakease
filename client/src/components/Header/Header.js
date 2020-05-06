efd88fbc5cd4b2ed4add961e20d86e694a4968a4
Dayna Knight <dknighted11@gmail.com>
2020-05-05 20:07:10 -0400

    begun the react elements of app

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+
+function Jumbotron() {
+  return (
+    < Jumbotron fluid>
+    <Container>
+      <h1>Speak. Easy</h1>
+      <p>
+        Inhale. Exhale. Write.
+    </p>
+    </Container>
+</Jumbotron >
+
+  )
+}
+
+export default Jumbotron;
---
+++ client/src/pages/Welcome.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Welcome () {
+  return (
+    <h1></h1>
+  )
+}
